8e05538e43aaf390518df95ae4e7d2af1ef2bb81
Hamiyet Özdemir <hmytt.9@gmail.com>
2021-08-03 10:02:39 +0300

    Initial Commit

---
+++ gitDeneme1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitDeneme1
+//
+//  Created by Mac on 3.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
