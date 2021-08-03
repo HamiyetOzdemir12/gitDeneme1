989b6886b1ee932cc01889f0fc54fb656fe69fa8
Hamiyet Özdemir <hmytt.9@gmail.com>
2021-08-03 10:07:27 +0300

    deneme2

--- gitDeneme1/ViewController.swift
+++ gitDeneme1/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     print("deneme github 1")
+        print("deneme github 2")
     }
 
 
